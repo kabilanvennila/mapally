b4aa4cda6958b735d220b9702b4fcecad246cbd1
kabilanvennila <kabilanvennila@gmail.com>
2021-09-24 21:32:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Mapp/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
